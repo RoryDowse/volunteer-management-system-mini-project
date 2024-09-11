c0fa8753cb15869170d3942ed51114450c6dd068
Rory Dowse <rorydowse@hotmail.com>
2024-09-11 14:18:58 -0700

    complete work.ts

--- server/src/models/work.ts
+++ server/src/models/work.ts
@@ -19,13 +19,43 @@ export class Work extends Model<WorkAttributes, WorkCreationAttributes> implemen
     public status!: string;
     public description!: string;
     public assignedVolunteerId?: number;
-    public readonly assignedVolunteer?: Volunteer;
-    public static associations: {
-      assignedVolunteer: Association<Work, Volunteer>;
-    };
+    public readonly assignedVolunteer?: Volunteer; 
 }
 
 export function WorkFactory(sequelize: Sequelize): typeof Work {
   // TODO: Initialize the Work Model
+  Work.init({
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+      allowNull: false
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    status: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    assignedVolunteerId: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      references: {
+        model: 'volunteers',
+        key: 'id'
+      },
+    },
+  }, {
+    sequelize,
+    modelName: 'Work',
+    timestamps: false,
+  });
+
   return Work;
 }
